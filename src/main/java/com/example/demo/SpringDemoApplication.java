15fd795df217e330de42e60cdb2a694b98dcd4c0
vs-1804 <vs1804769@gmail.com>
2024-07-01 15:45:35 +0530

    first push

--- src/main/java/com/example/demo/SpringDemoApplication.java
+++ src/main/java/com/example/demo/SpringDemoApplication.java
@@ -4,10 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDemoApplication {
-
+public class SpringDemoApplication{
+	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
+	
 	}
+
+	
 
 }
---
+++ src/main/java/com/example/demo/config/Car.java
@@ -0,0 +1,12 @@
+package com.example.demo.config;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Car {
+
+	public Car(String cls1) {
+		System.out.println("Car");
+	}
+
+}
